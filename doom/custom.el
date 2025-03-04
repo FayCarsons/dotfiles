c15e91de3cf4b5acdb2ffc71574921009b27a212
Fay Carsons <faycarsons@Mac.fios-router.home>
2025-03-03 20:06:22 -0500

    font fixes, install OCaml

--- doom/custom.el
+++ doom/custom.el
@@ -5,7 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(cider-font-lock-dynamically '(macro function var deprecated core))
  '(custom-safe-themes
-   '("48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" default)))
+   '("48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" default))
+ '(package-selected-packages '(reason-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- fish/config.fish
+++ fish/config.fish
@@ -20,3 +20,12 @@ fish_add_path -a ~/.config/doom/bin
 fish_add_path -a ~/.config/emacs/bin
 
 eval (/opt/homebrew/bin/brew shellenv)
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/Users/faycarsons/.opam/opam-init/init.fish' && source '/Users/faycarsons/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
